8ef2fb24b06520e6e824ba94ffb4dd1d0233235c
Voskal <andrei.daniel.danila@gmail.com>
2025-09-03 15:39:45 +0300

    Adding player.py and updating main.py

--- main.py
+++ main.py
@@ -1,16 +1,20 @@
 import pygame
 from constants import *
+from player import Player
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     time = pygame.time.Clock()
+    player = Player(x=SCREEN_WIDTH/2, y=SCREEN_HEIGHT/2)
     dt = 0
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+            
         screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
         dt = time.tick(60) / 1000
         
